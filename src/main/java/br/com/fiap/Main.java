a9983c9db05d59d43c9d4b4873233a327004723e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-13 23:00:36 -0300

    main rodando

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,14 +1,37 @@
 package br.com.fiap;
 
+import br.com.fiap.domain.entity.Author;
+import br.com.fiap.domain.entity.Book;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.UUID;
+import java.util.random.RandomGenerator;
 
 public class Main {
     public static void main(String[] args) {
 
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("oracle");
         EntityManager manager = factory.createEntityManager();
+
+        // var ?? agora pode aff
+        var bene = new Author(null, "Benefrancis");
+        var raquel = new Author(null, "Raquel");
+        var gulherme = new Author(null, "Guilherme");
+
+        var livro = new Book();
+        livro.setId(null);
+        livro.setNome("Java mapeamento Objeto relacional");
+        livro.setLaunch(LocalDateTime.now());
+        livro.setISBN(UUID.randomUUID().toString());
+        livro.addAuthor(raquel);
+        livro.addAuthor(gulherme);
+        livro.addAuthor(bene);
+
+
 
         factory.close();
         manager.close();
