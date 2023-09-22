c5f3d9972ff7bccea2c9e13c9ca4dc45838108fb
Benefrancis <benefrancis@gmail.com>
2023-09-22 19:53:42 -0300

    Start do servidor

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,77 +1,46 @@
 package br.com.fiap;
 
-import br.com.fiap.domain.entity.*;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.*;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
+
+    private static final String BASE_URI = "http://localhost/";
+
+    /**
+     * Método responsavel por configurar e fornecer um servidor HTTP
+     *
+     * @return
+     */
+    private static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig()
+                .packages( "br.com.fiap.domain.resources" );
+        return GrizzlyHttpServerFactory.createHttpServer( URI.create( BASE_URI ), rc );
+    }
+
     public static void main(String[] args) {
+        final HttpServer server = startServer();
+        System.out.println(
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory( "oracle", getProperties() );
-        EntityManager manager = factory.createEntityManager();
+                String.format( "Bookstore app started with endpoints available " +
+                        "as %s%nHit Ctrl-C to stop it....", BASE_URI )
+        );
 
-        var holding = new PessoaJuridica();
-        var bene = new PessoaFisica();
-        var vinicius = new PessoaFisica();
-
-        holding.setCnpj( UUID.randomUUID().toString() )
-                .setId( null )
-                .setNome( "Holding Benezinho SA" )
-                .setNascimento( LocalDate.now().minusYears( 5 ) );
-
-        bene.setCpf(UUID.randomUUID().toString() )
-                .setId( null )
-                .setNome( "Benefrancis do Nascimento" )
-                .setNascimento( LocalDate.of( 1977, 3, 8 ) );
-
-        vinicius.setCpf( UUID.randomUUID().toString() )
-                .setId( null )
-                .setNome( "Vinicius Cruzeiro Barbosa" )
-                .setNascimento( LocalDate.now().minusYears( 17 ) );
-
-        var authorBene = new Author();
-        authorBene.setPessoa( bene );
-
-        var authorVinicius = new Author();
-        authorVinicius.setPessoa( vinicius );
-
-        var livro = new Book();
-        livro.setISBN( UUID.randomUUID().toString() )
-                .setName( "Memórias de Benezinho" )
-                .setLaunch( LocalDateTime.now() )
-                .addAuthor( authorBene )
-                .addAuthor( authorVinicius );
-
-        manager.getTransaction().begin();
-        manager.persist( holding );
-        manager.persist( livro );
-        manager.getTransaction().commit();
-
-        System.out.println( holding );
-        System.out.println( livro );
-
-        Main main = new Main();
-
-        main.findAll(manager);
-
-        manager.close();
-        factory.close();
-
+        try {
+            System.in.read();
+            server.stop();
+        } catch (IOException e) {
+            throw new RuntimeException( e );
+        }
     }
 
-
-    public  void findAll(EntityManager manager){
-        manager.createQuery("FROM Pessoa").getResultList().forEach(System.out::println);
-    }
 
     private static Map<String, Object> getProperties() {
         Map<String, String> env = System.getenv();
@@ -79,10 +48,10 @@ public class Main {
 
         for (String chave : env.keySet()) {
             if (chave.contains( "USER_FIAP" )) {
-                properties.put( "jakarta.persistence.jdbc.user",  env.get( chave ) );
+                properties.put( "jakarta.persistence.jdbc.user", env.get( chave ) );
             }
             if (chave.contains( "PASSWORD_FIAP" )) {
-                properties.put( "jakarta.persistence.jdbc.password",  env.get( chave ) );
+                properties.put( "jakarta.persistence.jdbc.password", env.get( chave ) );
             }
             // Outras configurações de propriedade ....
         }
